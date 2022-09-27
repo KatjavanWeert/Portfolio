b35814b9381d052a2dac8706f78bee03526f6af0
KatjavanWeert <44201968+KatjavanWeert@users.noreply.github.com>
2022-09-27 14:42:03 +0200

    add photos, change carousel arrows

--- pages/contact.js
+++ pages/contact.js
@@ -13,20 +13,54 @@ export default function Contact() {
       <main>
         <div className="text-container">
           <h1>Contact</h1>
+          <div>
+            <img
+              src="/process2.jpg"
+              style={{ width: "20rem", clipPath: "circle()" }}
+            ></img>
+          </div>
           <h2>Can I help you or your organization in some way?</h2>
           <p>
             Just send me a whatsapp message, e-mail, or give me a call and we
-            can discuss the option over a cup of coffee!
+            can discuss the possibilities over a cup of coffee!
           </p>
-          <h3>E-mail</h3>
-          <a href="mailto:katjavanweert@gmail.com">katjavanweert@gmail.com</a>
-          <h3>Phone</h3>
-          <a href="tel:+31610335753">+31 6 10 33 57 53</a>
-          <h3>Social Media</h3>
-          <a href="https://www.linkedin.com/in/katja-van-weert-374114b1/">
-            LinkedIn
-          </a>
-          <a href="https://www.instagram.com/katjavanweert/">Instagram</a>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              gap: "4rem",
+              textAlign: "center",
+              width: "80%",
+            }}
+          >
+            <div style={{ flex: "33%" }}>
+              <h5>E-mail</h5>
+              <a href="mailto:katjavanweert@gmail.com">
+                katjavanweert@gmail.com
+              </a>
+            </div>
+            <div style={{ flex: "33%" }}>
+              <h5>Phone</h5>
+              <a href="tel:+31610335753">+31 6 10 33 57 53</a>
+            </div>
+            <div style={{ flex: "33%" }}>
+              <h5>Social Media</h5>
+              <a
+                target="_blank"
+                href="https://www.linkedin.com/in/katja-van-weert-374114b1/"
+              >
+                LinkedIn
+              </a>
+              <br />
+              <a
+                target="_blank"
+                href="https://www.instagram.com/katjavanweert/"
+              >
+                Instagram
+              </a>
+            </div>
+          </div>
         </div>
       </main>
 
